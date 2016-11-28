67cf24285137753af2233cd7ad1a94c5a9380e43
Nikolay Amiantov <ab@fmap.me>
2016-11-28 14:31:11 +0300

    Add default nix.conf
    By default Nix doesn't use cache at all and tries to build everything.

--- scripts/local.mk
+++ scripts/local.mk
@@ -6,8 +6,10 @@ nix_noinst_scripts := \
 noinst-scripts += $(nix_noinst_scripts)
 
 profiledir = $(sysconfdir)/profile.d
+nixconfdir = $(sysconfdir)/nix
 
 $(eval $(call install-file-as, $(d)/nix-profile.sh, $(profiledir)/nix.sh, 0644))
 $(eval $(call install-file-as, $(d)/nix-profile-daemon.sh, $(profiledir)/nix-daemon.sh, 0644))
+$(eval $(call install-file-as, $(d)/nix.conf, $(nixconfdir)/nix.conf, 0644))
 
 clean-files += $(nix_noinst_scripts)
